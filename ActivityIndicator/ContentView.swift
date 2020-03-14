de33ed35ec32584d3b7fe693453e1da6f3928e9e
Pavel Vaitsikhouski <pavel.vaitsikhouski@schibsted.com>
2020-03-14 13:17:35 +0300

    Initial Commit

---
+++ ActivityIndicator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ActivityIndicator
+//
+//  Created by Pavel Vaitsikhouski on 3/14/20.
+//  Copyright © 2020 Pavel Vaitsikhouski. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
